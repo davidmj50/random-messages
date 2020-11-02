dbcc3b868476847b2a315588209d81f0384e8d77
davidmj950 <davidmj950@gmail.com>
2020-11-02 15:25:32 -0500

    Random messages JS

--- bin/global.js
+++ bin/global.js
@@ -2,3 +2,8 @@
 let random = require('../src/index.js')
 
 random.randomMsg();
+
+//poner en la terminal
+//sudo npm link
+//sudo npm install -g /home/david/Documents/platzi/javascript/random-messages
+//random-messages
